7d6e7df5de9d7ad7c4aecdd443e11f3f94dd1aa3
vivek <vivek.bnb@gmail.com>
2022-06-09 09:45:10 +0530

    task demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  loginform:any;
+
+  ngOnInit(){
+  this.loginform= new  FormGroup({
+    firstname : new FormControl(null,Validators.required),
+    lastname : new FormControl(null,Validators.required),
+    email : new FormControl(null,[Validators.required,Validators.email]),
+    Phonenumber : new FormControl(null,[Validators.required, Validators.maxLength(10)]),
+
+  })
+
+  }
+  get firstname(){ return this.loginform.get('firstname');}
+  get lastname(){ return this.loginform.get('lastname');}
+  get email(){ return this.loginform.get('email');}
+  get Phonenumber(){ return this.loginform.get('Phonenumber');}
+
+  submit(data:any){
+    console.log(this.loginform.value);
+    let datas = JSON.parse(localStorage.getItem('logindata') ?? `[]`);
+    let currentUser = datas.find((user:any)=>user.email === data.email);
+    if (currentUser?.email){
+      alert('user exits');
+    }else{
+      datas.push(data);
+      localStorage.setItem("logindata",JSON.stringify(datas));
+
+    }
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{ReactiveFormsModule,FormsModule} from '@angular/forms';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ReactiveFormsModule,FormsModule
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
